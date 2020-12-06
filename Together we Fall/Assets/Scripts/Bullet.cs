20a23f3341a411da644690fe732c2cf43483f984
Victor Giovannoni <vernalhav@usp.br>
2020-12-06 00:20:05 -0300

    Fixed bullets only hitting soldiers (should refactor)

--- Together we Fall/Assets/Scripts/Bullet.cs
+++ Together we Fall/Assets/Scripts/Bullet.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     private float damage;
-    [SerializeField] private Rigidbody2D rigidBody = null;
+    [SerializeField] private Rigidbody2D rigidBody;
     private string owner;
-
-    void Start()
-    {
-        // rigidBody = GetComponent<Rigidbody2D>();
-    }
 
     public virtual void Shoot(Transform _target, float _speed, float _dmg, string _owner){
         Vector3 _dir = _target.position - transform.position;
@@ -20,9 +15,13 @@ public class Bullet : MonoBehaviour
         rigidBody.velocity = direction * _speed;
         damage = _dmg;
     }
-
+    
     void OnTriggerEnter2D(Collider2D other){
-        if(other.gameObject.CompareTag("Soldier") && owner != "Soldier")
+        // TODO: Refactor!
+        if(owner != "Soldier" && (other.gameObject.CompareTag("Soldier") || 
+                                    other.gameObject.CompareTag("Tank") || 
+                                    other.gameObject.CompareTag("Runner") || 
+                                    other.gameObject.CompareTag("Irene")))
         {
             other.gameObject.GetComponentInParent<Soldier>().ReceiveDamage(damage);
             Destroy(gameObject);
