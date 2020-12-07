85a8413f0cc026ff17d2205c857db3dbca90c6e5
Victor Giovannoni <vernalhav@usp.br>
2020-12-06 23:42:41 -0300

    Implemented rudimentary sniper animation

--- Together we Fall/Assets/Scripts/Combatents/Sniper.cs
+++ Together we Fall/Assets/Scripts/Combatents/Sniper.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Sniper : Enemy
 {
+    [SerializeField] LineRenderer shot;
+    [SerializeField] Transform firePoint;
 
     public override void Attack(Transform targetPosition)
     {
         Debug.Log("POOWWW!");
         targetPosition.GetComponent<Soldier>().ReceiveDamage(damage);
+
+        shot.SetPosition(0, firePoint.position);
+        shot.SetPosition(1, targetPosition.position);
+        shot.widthMultiplier = 0.1f;
+        DOTween.To( () => shot.widthMultiplier,  (float x) => shot.widthMultiplier = x, 0f, 1f).SetEase(Ease.OutQuad);
     }
 }
