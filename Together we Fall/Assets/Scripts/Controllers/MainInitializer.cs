60aac0d7593dbd67a395ee4edf814c7f12638d82
jpsecundino <39441841+jpsecundino@users.noreply.github.com>
2020-12-10 07:45:43 -0300

    preparing for permadeath

--- Together we Fall/Assets/Scripts/Controllers/MainInitializer.cs
+++ Together we Fall/Assets/Scripts/Controllers/MainInitializer.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class MainInitializer : MonoBehaviour
 {
     [SerializeField] private MainGameContent mainGameContent;
+    [SerializeField] private Deck deck;
+    //runner soldier tank irene
+    [SerializeField] private List<int> cardCounts;
+
+    void Start()
+    {
+        InitializeDeck();       
+    }
+
     void Awake()
     {
         SceneTracker.sceneArgs = new Queue<SceneArgs>();
 
         foreach(SceneArgs sceneArgs in mainGameContent.mainGameContent){
             SceneTracker.sceneArgs.Enqueue(sceneArgs);
+        }
+    }
+
+    public void InitializeDeck(){
+        for(int i = 0; i < deck.cards.Count; i ++){
+            deck.ChangeCardAliveCount(i, cardCounts[i] ); 
         }
     }
 }
--- Together we Fall/Assets/Scripts/Deck.cs
+++ Together we Fall/Assets/Scripts/Deck.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class Deck : ScriptableObject
 {
     public List<Card> cards;
+
+
+    public void ChangeCardAliveCount(int idx, int count){
+        if(0 <= idx && idx < cards.Count){
+            cards[idx].aliveCounter = count;
+    	    EditorUtility.SetDirty(cards[idx]);
+        }
+    }
+
+    public void SaveCards(){
+        foreach(Card c in cards){
+            EditorUtility.SetDirty(c);
+        }
+    }
 }
