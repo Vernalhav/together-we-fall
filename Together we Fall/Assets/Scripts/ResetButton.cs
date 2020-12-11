b94cd29f8a59241965a3bb1de1530a9a08392515
Victor Giovannoni <vernalhav@usp.br>
2020-12-11 00:54:42 -0300

    Added reset button

---
+++ Together we Fall/Assets/Scripts/ResetButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ResetButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private CanvasGroup resetButtonGroup;
+
+    void Start()
+    {
+        resetButtonGroup = GetComponent<CanvasGroup>();
+        resetButtonGroup.alpha = 0.5f;
+    }
+
+    public void OnPointerEnter(PointerEventData e)
+    {
+        resetButtonGroup.alpha = 1f;
+    }
+
+    public void OnPointerExit(PointerEventData e)
+    {
+        resetButtonGroup.alpha = 0.5f;
+    }
+}
